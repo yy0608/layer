24417bc5961dc6cb3f12613e2edd952ef7cf7e72
yy0608 <447462224@qq.com>
2018-03-02 10:09:29 +0800

    Update layer.js

--- layer.js
+++ layer.js
@@ -1,7 +1,7 @@
 ;(function (w) {
   var HTMLS = {
     toast: '<div class="J_CompToast comp-toast"></div>',
-    popup: '<div class="J_MaskCont mask-cont"><div class="J_WinpopBox winpop-box"><div class="J_WinpopTitle winpop-title">提示</div><div class="J_WinpopMain winpop-main"></div><div class="J_WinpopBtns winpop-btns"></div></div>',
+    popup: '<div class="J_MaskCont mask-cont mask-animate"><div class="J_WinpopBox winpop-box"><div class="J_WinpopTitle winpop-title">提示</div><div class="J_WinpopMain winpop-main"></div><div class="J_WinpopBtns winpop-btns"></div></div>',
     confirm: '<a href="javascript:;" class="J_AltBtn pop-btn alert-btn">确定</a>',
     prompt: '<a href="javascript:;" class="J_CfmFalse pop-btn cfm-false">取消</a><a href="javascript:;" class="J_CfmTrue pop-btn cfm-true">确定</a>',
     content: '<div class="J_MaskCont mask-cont"><div class="J_ContentBox content-box"><span class="J_CloseBtn close-btn"></span><div class="J_InnerContent inner-content"></div></div></div>',
@@ -85,7 +85,7 @@
     insertStyle: function () {
       var styleEle = document.createElement('style')
       var commonStyle = '\
-        input{outline:0;}\
+        input, textarea{outline:0;}\
         .comp-toast{position:fixed;z-index:999;padding:10px 20px;left:50%;font-size:16px;line-height:25px;border-radius:4px;background-color:#fff;pointer-events:none;}\
         .mask-cont{position:fixed;z-index:999;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,.5);}\
         .winpop-box{position:absolute;height:160px;left:50%;top:50%;border-radius:2px;background-color:#fff;font-size:14px;}\
@@ -99,12 +99,13 @@
         .input-context{display:flex;flex-grow:1;flex-wrap:wrap;}\
         .input-context select{outline:0;flex-grow:1;height:32px;line-height:32px;}\
         .input-item .input{padding:0;outline:0;text-indent:10px;height:28px;flex-grow:1;}\
+        .input-item .textarea{resize:none;width:100%;padding:10px;box-sizing:border-box;}\
         .require-star{position:absolute;color:red;left:-10px;}\
         .require-star.dis-none{display:none;}\
         .require-star.dis-block{display:block;}\
         .one-input{width:100%;padding:10px 0;outline:0;height:80px;resize:none;text-indent:10px;}\
         .loading-content{margin:100px auto;font-size:25px;width:1em;height:1em;border-radius:50%;position:relative;text-indent:-9999em;-webkit-animation:loading 1.1s infinite ease;animation:loading 1.1s infinite ease;-webkit-transform:translateZ(0);-ms-transform:translateZ(0);transform:translateZ(0);}\
-        .content-box{position:absolute;font-size:14px;left:50%;top:50%;box-sizing:border-box;background-color:#fff;border-radius:4px;overflow:hidden;}\
+        .content-box{position:absolute;font-size:14px;left:50%;top:50%;box-sizing:border-box;background-color:#fff;border-radius:4px;overflow:hidden;visibility:hidden;}\
         .close-btn{position:absolute;right:0;top:0;width:60px;height:60px;overflow:hidden;z-index:100000;cursor:pointer;}\
         .close-btn:hover::before,.close-btn:hover::after{background:#333;}\
         .close-btn::before,.close-btn::after{content:\'\';position:absolute;height:2px;width:50%;top:50%;left:50%;margin-top:-1px;margin-left:-25%;background-color:#888;}\
@@ -113,6 +114,29 @@
         .loading-cont{position:fixed;z-index:999;top:50%;left:50%;width:240px;border-radius:10px;background-color:rgba(0,0,0,.5);}\
         .loading-text{display:none;text-align:center;color:#eaeaea;font-size:28px;margin-top:-30px;line-height:60px;}\
         .mask-hide{background:rgba(0,0,0,0);}\
+        .mask-animate{opacity:0;animation:maskAnimate .2s ease forwards;-webkit-animation:maskAnimate .2s ease forwards;}\
+        @keyframes maskAnimate{\
+          0%{opacity:0;}\
+          100%{opacity:1;}\
+        }\
+        .layer-pc-animate{opacity:0;animation:layerPcAnimate .2s ease forwards;-webkit-animation:layerPcAnimate .2s ease forwards;}\
+        .layer-phone-animate{opacity:0;animation:layerPhoneAnimate .2s ease forwards;-webkit-animation:layerPhoneAnimate .2s ease forwards;}\
+        @keyframes layerPcAnimate{\
+          0%{opacity:0;transform:translateY(-30px);}\
+          100%{opacity:1;transform:translateY(0);}\
+        }\
+        @keyframes layerPhoneAnimate{\
+          0%{opacity:0;transform:translate3d(-50%,-50%,0) scale(.5);}\
+          100%{opacity:1;transform:translate3d(-50%,-50%,0) scale(1);}\
+        }\
+        @-webkit-keyframes layerPcAnimate{\
+          0%{opacity:0;transform:translateY(-30px);}\
+          100%{opacity:1;transform:translateY(0);}\
+        }\
+        @-webkit-keyframes layerPhoneAnimate{\
+          0%{opacity:0;transform:translate3d(-50%,-50%,0) scale(.5);}\
+          100%{opacity:1;transform:translate3d(-50%,-50%,0) scale(1);}\
+        }\
         @-webkit-keyframes loading{\
           0%,\
           100%{box-shadow:0em -2.6em 0em 0em #ffffff,1.8em -1.8em 0 0em rgba(255,255,255,0.2),2.5em 0em 0 0em rgba(255,255,255,0.2),1.75em 1.75em 0 0em rgba(255,255,255,0.2),0em 2.5em 0 0em rgba(255,255,255,0.2),-1.8em 1.8em 0 0em rgba(255,255,255,0.2),-2.6em 0em 0 0em rgba(255,255,255,0.5),-1.8em -1.8em 0 0em rgba(255,255,255,0.7);}\
@@ -139,16 +163,15 @@
       var pcStyle = '\
         .comp-toast{width:240px;top:60px;margin-left:-140px;text-align:justify;box-shadow:0 2px 12px 0 rgba(0,0,0,.1);border:1px solid #ebeef5;background-color:#fff;}\
         .winpop-box{width:260px;margin-left:-130px;margin-top:-178px;}\
-        .winpop-box-input{width:360px;margin-left:-180px;margin-top:0;height:auto;top:50%;}\
+        .winpop-box-input{width:360px;margin-left:-180px;height:auto;}\
         .pop-btn{display:inline-block;color:#333;border-radius:2px;height:28px;line-height:28px;margin:5px 5px 0;padding:0 15px;text-decoration:none;border:1px solid #E9E7E7;background-color:#fff;}\
         .alert-btn,.cfm-true{color:#fff;border-color:#1E9FFF;background-color:#1E9FFF;}\
-        .content-box{transform:translateX(-50%) translateY(-50%);-webkit-transform:translateX(-50%) translateY(-50%);-moz-transform:translateX(-50%) translateY(-50%);-ms-transform:translateX(-50%) translateY(-50%);-o-transform:translateX(-50%) translateY(-50%);}\
         .loading-cont{transform:translateX(-50%) translateY(-50%) scale(.5);-webkit-transform:translateX(-50%) translateY(-50%) scale(.5);-moz-transform:translateX(-50%) translateY(-50%) scale(.5);-ms-transform:translateX(-50%) translateY(-50%) scale(.5);-o-transform:translateX(-50%) translateY(-50%) scale(.5);}\
       '
       var mobileStyle = '\
-        .comp-toast{text-align:center;width:60%;transform:translate3d(-50%,-50%,0);-webkit-transform:translate3d(-50%,-50%,0);box-sizing:border-box;top:50%;color:#fff;background-color:rgba(0,0,0,.65);}\
+        .comp-toast{text-align:center;max-width:60%;transform:translate3d(-50%,-50%,0);-webkit-transform:translate3d(-50%,-50%,0);box-sizing:border-box;top:50%;color:#fff;background-color:rgba(0,0,0,.65);}\
         .winpop-box{display:flex;flex-direction:column;width:80%;transform:translate3d(-50%,-50%,0);-webkit-transform:translate3d(-50%,-50%,0);}\
-        .winpop-box-input{margin-top:0;height:auto;top:50%;transform:translate3d(-50%,0,0);-webkit-transform:translate3d(-50%,0,0);}\
+        .winpop-box-input{margin-top:0;height:auto;top:50%;transform:translate3d(-50%,-50%,0);-webkit-transform:translate3d(-50%,-50%,0);}\
         .winpop-title{display:none;}\
         .winpop-main{text-align:center;flex-grow:1;font-size:16px;}\
         .winpop-btns{display:flex;}\
@@ -174,7 +197,9 @@
     toastFunc: function (content, interval, cb) {
       w.clearTimeout(this.timer)
       this.get('body').appendChildAdvanced(HTMLS.toast)
+      var plat = utils.isPc() ? 'layer-pc-animate' : 'layer-phone-animate'
       var $J_CompToast = utils.select('.J_CompToast')
+      $J_CompToast.className = 'J_CompToast comp-toast ' + plat
       $J_CompToast.innerHTML = content
       this.timer = setTimeout(function () {
         utils.remove('.J_CompToast')
@@ -190,6 +215,8 @@
       this.get('body').appendChildAdvanced(HTMLS.popup)
       utils.select('.J_WinpopMain').innerHTML = content
       utils.select('.J_WinpopBtns').appendChildAdvanced(HTMLS.confirm)
+      var plat = utils.isPc() ? 'layer-pc-animate' : 'layer-phone-animate'
+      utils.select('.J_WinpopBox').className = 'J_WinpopBox winpop-box ' + plat
       utils.select('.J_WinpopTitle').innerHTML = title || '提示'
       utils.select('.J_AltBtn').innerHTML = cfmTxt || '确定'
       confirmLayer = utils.select('.J_MaskCont')
@@ -209,6 +236,8 @@
       this.get('body').appendChildAdvanced(HTMLS.popup)
       utils.select('.J_WinpopMain').innerHTML = str
       utils.select('.J_WinpopBtns').appendChildAdvanced(HTMLS.prompt)
+      var plat = utils.isPc() ? 'layer-pc-animate' : 'layer-phone-animate'
+      utils.select('.J_WinpopBox').className = 'J_WinpopBox winpop-box ' + plat
       promptLayer = utils.select('.J_MaskCont')
       this.set('promptLayer', promptLayer)
       this.addPromptEvent(cb)
@@ -243,9 +272,16 @@
         clickMaskHide = params.clickMaskHide || false
       }
       this.get('body').appendChildAdvanced(HTMLS.content)
+      var plat = utils.isPc() ? 'layer-pc-animate' : 'layer-phone-animate'
       utils.select('.J_InnerContent').innerHTML = content
       utils.select('.J_ContentBox').style.width = width
       utils.select('.J_ContentBox').style.height = height
+      if (utils.isPc()) {
+        utils.select('.J_ContentBox').style.marginLeft = - (utils.select('.J_ContentBox').offsetWidth / 2) + 'px'
+        utils.select('.J_ContentBox').style.marginTop = - (utils.select('.J_ContentBox').offsetHeight / 2)  + 'px'
+      }
+      utils.select('.J_ContentBox').style.visibility = 'visible'
+      utils.select('.J_ContentBox').className = 'J_ContentBox content-box ' + plat
       this.addContentEvent(clickMaskHide, cb)
     },
     contentRemove: function () {
@@ -289,12 +325,14 @@
     },
     input: function (options) {
       this.get('body').appendChildAdvanced(HTMLS.popup)
-      utils.select('.J_WinpopBox').className = 'J_WinpopBox winpop-box winpop-box-input'
+      var plat = utils.isPc() ? 'layer-pc-animate' : 'layer-phone-animate'
+      utils.select('.J_WinpopBox').className = 'J_WinpopBox winpop-box winpop-box-input ' + plat
       var title = options && options.title || '提示'
       var confirmText = options && options.confirmText || '确认'
       var cancelText = options && options.cancelText || '取消'
       var inputs = options && options.inputs
       var cb = options && options.callback
+      var labelWidth = options && options.labelWidth
       var inputsStr = ''
       utils.select('.J_WinpopTitle').innerHTML = title
       if (inputs) {
@@ -303,6 +341,9 @@
             case undefined:
             case 'text':
               inputsStr += '<div class="input-item"><span class="require-star ' + (inputs[i].required ? 'dis-block' : 'dis-none') + '">*</span><div class="label">' + inputs[i].label + '</div><input class="input" ' + (inputs[i].placeholder ? 'placeholder="' + inputs[i].placeholder + '"' : '') + ' name="' + inputs[i].name + '" value="' + (inputs[i].value || '') + '"></div>'
+              break;
+            case 'textarea':
+              inputsStr += '<div class="input-item"><span class="require-star ' + (inputs[i].required ? 'dis-block' : 'dis-none') + '">*</span><div class="label">' + inputs[i].label + '</div><textarea rows="' +(inputs[i].rows ? inputs[i].rows : 3) + '" class="textarea" ' + (inputs[i].placeholder ? 'placeholder="' + inputs[i].placeholder + '"' : '') + ' name="' + inputs[i].name + '">' + (inputs[i].value || '') + '</textarea></div>'
               break;
             case 'radio':
               inputsStr += '<div class="input-item"><span class="require-star ' + (inputs[i].required ? 'dis-block' : 'dis-none') + '">*</span><div class="label">' + inputs[i].label + '</div><div class="input-context">';
@@ -347,10 +388,16 @@
         inputsStr += '<textarea class="one-input" name="one" rows="5"></textarea>'
       }
       utils.select('.J_WinpopMain').appendChildAdvanced(inputsStr)
+      var inputLabel = utils.selectAll('.winpop-box-input .input-item .label')
+      if (inputLabel.length) {
+        for (var i = 0; i < inputLabel.length; i++) {
+          inputLabel[i].style.width = labelWidth || '60px'
+        }
+      }
       utils.select('.J_WinpopBtns').appendChildAdvanced(HTMLS.input)
       utils.select('.J_CfmFalse').innerHTML = cancelText
       utils.select('.J_CfmTrue').innerHTML = confirmText
-      utils.select('.J_WinpopBox').style.marginTop = -utils.select('.J_WinpopBox').offsetHeight / 2 + 'px'
+      // utils.select('.J_WinpopBox').style.marginTop = -utils.select('.J_WinpopBox').offsetHeight / 2 + 'px'
       this.addInputEvent({
         cb: cb,
         inputs: inputs,
@@ -381,6 +428,7 @@
             switch (inputs[i].type) {
               case undefined:
               case 'text':
+              case 'textarea':
                 tempRes[name] = utils.select('[name="' + name + '"]').value
                 break;
               case 'radio':
@@ -405,7 +453,7 @@
             }
             if (inputs[i].required) {
               if ((typeof tempRes[name] === 'object' && !tempRes[name].length) || (typeof tempRes[name] === 'string' && !tempRes[name])) {
-                if (!inputs[i].type || inputs[i].type === 'text') {
+                if (!inputs[i].type || inputs[i].type === 'text' || inputs[i].type === 'textarea') {
                   _this.toast('请填写' + inputs[i].label)
                 } else {
                   _this.toast('请选择' + inputs[i].label)
